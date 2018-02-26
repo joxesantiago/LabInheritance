d657329bbcb1d82bec8a7f45ec66930f904e7c81
ICOM STUDENTS <jose.santiago90@upr.edu>
2018-02-26 13:33:55 -0400

    LabInheritance

---
+++ InheritanceLabStudents/src/orderedStructures/Arithmetic.java
@@ -0,0 +1,34 @@
+package orderedStructures;
+
+import java.nio.channels.IllegalSelectorException;
+
+public class Arithmetic extends Progression {
+	private double commonDifference; 
+	
+	public Arithmetic(double firstValue, double commonDifference) { 
+		super(firstValue); 
+		this.commonDifference = commonDifference; 
+		firstValueT=true;
+	}
+	
+	@Override
+	public double nextValue() throws IllegalStateException{
+		if (!firstValueT) 
+			throw new IllegalStateException("Set first value");
+		current = current + commonDifference; 
+		return current;
+	}
+	
+	@Override
+	public String toString(){
+		return "Arith("+(int)firstValue()+","+(int)this.commonDifference+")" ;
+	}
+	
+	@Override
+	public double getTerm(int n) throws IndexOutOfBoundsException {
+		if (n <= 0) 
+			throw new IndexOutOfBoundsException("printAllTerms: Invalid argument value = " + n); 
+
+		return firstValue()+commonDifference*(n-1);
+	}
+}
---
+++ InheritanceLabStudents/src/orderedStructures/Fibonacci.java
@@ -0,0 +1,33 @@
+package orderedStructures;
+
+public class Fibonacci extends Progression {
+
+	private double prev; 
+	
+	public Fibonacci() { 
+		this(1); 
+		prev = 0; 
+	}
+	private Fibonacci(double first) {
+		super(first);
+	}
+
+	@Override
+	public double nextValue() throws IllegalStateException{
+		if (!firstValueT) 
+			throw new IllegalStateException("Set first value");
+		current= prev+current;
+		prev=current-prev;
+		
+		return current;
+		
+	}
+	
+	@Override	
+	public double firstValue() { 
+		double value = super.firstValue(); 
+		prev = 0; 
+		return value; 
+	}
+
+}
